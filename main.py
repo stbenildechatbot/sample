a5a0186b5ce1978f4deff9b5ef5e50e937b40fd6
stbenildechatbot <stbenildechatbot@gmail.com>
2024-11-27 14:13:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import streamlit as st
 import time
 import backend
 
-# Streamed response emulator
 def response_generator(prompt):
-    response = backend.GenerateResponse(prompt)
-    for word in response.split():
-        yield word + " "  
-        time.sleep(0.05)
+    try:
+        response = backend.GenerateResponse(prompt)
+        lines = response.split('\n')
+        for line in lines:
+            words = line.split()
+            for word in words:
+                yield word + " "
+                time.sleep(0.05)
+            yield "\n"  # Preserve line breaks
+    except Exception as e:
+        yield f"An error occurred: {str(e)}"
 
 st.title("BND CHATBOT")
 
@@ -21,7 +27,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("school helpdesk"):
+if prompt := st.chat_input("HI! I'm BND Chatbot, what is your question?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
